395254b7df4a3d75a259b545dd49eb16007bdcac
Annika Hannig <annika@hannig.cc>
2020-08-17 15:46:25 +0200

    add env for config files

--- cmd/b3scaled/main.go
+++ cmd/b3scaled/main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"gitlab.com/infra.run/public/b3scale/pkg/cluster"
 	"gitlab.com/infra.run/public/b3scale/pkg/config"
 )
 
+// Get configuration from environment with
+// a default fallback.
+func getopt(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
+
 func main() {
 	banner() // Most important.
 	log.Println("Starting b3scaled.")
 
 	// Parse flags
-	frontendsConfigFilename := "etc/b3scale/frontends.conf"
-	backendsConfigFilename := "etc/b3scale/backends.conf"
+	frontendsConfigFilename := getopt(
+		"B3SCALE_FRONTENDS", "etc/b3scale/frontends.conf")
+	backendsConfigFilename := getopt(
+		"B3SCALE_BACKENDS", "etc/b3scale/backends.conf")
+
+	log.Println("Using frontends from:", frontendsConfigFilename)
+	log.Println("Using backends from:", backendsConfigFilename)
 
 	// Initialize configuration
 	backendsConfig := config.NewBackendsFileConfig(
