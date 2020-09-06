913e4df6818f8cda3f00564c05b7b79523fafc44
Annika Hannig <annika@hannig.cc>
2020-09-06 11:03:14 +0200

    added documentation

--- pkg/middlewares/middleware.go
+++ pkg/middlewares/middleware.go
@@ -10,12 +10,21 @@ type StateUpdate map[string]interface{}
 
 // The State of the middleware
 type State interface {
+	// Apply a state update.
 	Update(next StateUpdate) error
+
+	// Schema creates a mapping of acceptable
+	// keys and datatypes in an update.
 	Schema() map[string]string
+
+	// ID indentifies the state, so it
+	// can be queried from an API
+	ID() string
 }
 
-// HandlerFunc accepts a bbb request and creates a bbb response
-type HandlerFunc func(*bbb.Request) (*bbb.Response, error)
+// HandlerFunc accepts a bbb request and state. It produces
+// a bbb response or an error.
+type HandlerFunc func(*bbb.Request, State) (*bbb.Response, error)
 
 // A Middleware is a function, accepting a next handler function
 // and returning a handler function and an initial state.
