c6a7685a1fdff1d60f16385a23d8cc56bd9bc23f
Annika Hannig <annika@hannig.cc>
2020-09-28 20:28:03 +0200

    removed unused import

--- cmd/b3scaled/main.go
+++ cmd/b3scaled/main.go
@@ -8,7 +8,7 @@ import (
 	"gitlab.com/infra.run/public/b3scale/pkg/config"
 	"gitlab.com/infra.run/public/b3scale/pkg/iface/http"
 	"gitlab.com/infra.run/public/b3scale/pkg/middlewares/requests"
-	"gitlab.com/infra.run/public/b3scale/pkg/middlewares/routing"
+	// "gitlab.com/infra.run/public/b3scale/pkg/middlewares/routing"
 )
 
 // Get configuration from environment with
