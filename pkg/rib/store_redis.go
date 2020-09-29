8de3b2c36145c684274c4abba0f0079d24cfa6f8
Annika Hannig <annika@hannig.cc>
2020-09-29 17:30:38 +0200

    initial redis store

--- pkg/rib/store_redis.go
+++ pkg/rib/store_redis.go
@@ -2,10 +2,24 @@ package rib
 
 import (
 	"github.com/go-redis/redis/v8"
+	"gitlab.com/infra.run/public/b3scale/pkg/cluster"
 )
 
 // A RedisStore is a implemenation of a Store
 // interface using redis.
 type RedisStore struct {
-	rdb *redis.Client
+	state *cluster.State
+	rdb   *redis.Client
+}
+
+// NewRedisStore makes a new store with
+// a redis host address and a cluster state.
+func NewRedisStore(state *cluster.State, addr string) *RedisStore {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+	return &RedisStore{
+		state: state,
+		rdb:   rdb,
+	}
 }
