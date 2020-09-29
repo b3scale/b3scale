973f545337d74cf3759d09971a3ff9bd0da48e7a
Annika Hannig <annika@hannig.cc>
2020-09-30 00:07:58 +0200

    get/set backend

--- pkg/rib/redis.go
+++ pkg/rib/redis.go
@@ -1,8 +1,13 @@
 package rib
 
 import (
+	//	"context"
+	"context"
+	"fmt"
+
 	"github.com/go-redis/redis/v8"
 
+	"gitlab.com/infra.run/public/b3scale/pkg/bbb"
 	"gitlab.com/infra.run/public/b3scale/pkg/cluster"
 )
 
@@ -24,4 +29,44 @@ func NewRedisClusterStore(
 		state: state,
 		rdb:   rdb,
 	}
+}
+
+// GetBackend retrieves a backend associated with
+// a Meeting from the store.
+// If no meeting was found, nil will be returned.
+func (s *RedisClusterStore) GetBackend(
+	meeting *bbb.Meeting,
+) (*cluster.Backend, error) {
+	// Check identifier
+	if meeting.MeetingID == "" {
+		return nil, fmt.Errorf("meeting id is empty")
+	}
+	ctx := context.Background()
+	// Lookup backend id in cache
+	id, err := s.rdb.Get(ctx, meeting.MeetingID).Result()
+	if err != nil {
+		return nil, err
+	}
+	if id == "" {
+		return nil, nil
+	}
+	backend := s.state.GetBackendByID(id)
+	return backend, nil
+}
+
+// SetBackend associates a backend with a meeting
+func (s *RedisClusterStore) SetBackend(
+	meeting *bbb.Meeting, backend *cluster.Backend,
+) error {
+	// Check identifiers
+	if meeting.MeetingID == "" {
+		return fmt.Errorf("meeting id is empty")
+	}
+	if backend.ID == "" {
+		return fmt.Errorf("backend id is empty")
+	}
+
+	ctx := context.Background()
+
+	return s.rdb.Set(ctx, meeting.MeetingID, backend.ID, 0).Err()
 }
