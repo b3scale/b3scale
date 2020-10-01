6e592b843fe68e78a6e4cf5f7fc1454615d53bf4
Annika Hannig <annika@hannig.cc>
2020-10-01 14:44:51 +0200

    initial test

--- pkg/cluster/rib_redis_test.go
+++ pkg/cluster/rib_redis_test.go
@@ -2,7 +2,10 @@ package cluster
 
 import (
 	"testing"
-	// "gitlab.com/infra.run/public/b3scale/pkg/bbb"
+
+	"github.com/go-redis/redis/v8"
+
+	"gitlab.com/infra.run/public/b3scale/pkg/bbb"
 )
 
 func makeClusterState() *State {
@@ -19,7 +22,33 @@ func makeClusterState() *State {
 	return state
 }
 
+func makeRedisRIB(s *State) *RedisClusterRIB {
+	return NewRedisClusterRIB(s, &redis.ClusterOptions{
+		Addrs: []string{":6379"},
+	})
+}
+
 func TestGetBackend(t *testing.T) {
 	state := makeClusterState()
+	rib := makeRedisRIB(state)
+
+	// Associate meeting
+	b1 := state.GetBackendByID("backend1")
+	m := &bbb.Meeting{
+		MeetingID: "meeting1",
+	}
+
+	res, err := rib.GetBackend(m)
+	if err != nil {
+		t.Error(err)
+	}
+	if res != nil {
+		t.Error("Unexpected result:", res)
+	}
+
+	err = rib.SetBackend(m, b1)
+	if err != nil {
+		t.Error(err)
+	}
 
 }
