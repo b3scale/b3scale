0f5c67553fef443d0ec4cfeec322dab9c1bd101f
Annika Hannig <annika@hannig.cc>
2020-10-29 23:53:16 +0100

    show backends... testing...

--- cmd/b3scalectl/main.go
+++ cmd/b3scalectl/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -27,9 +28,20 @@ func main() {
 		log.Fatal(err)
 	}
 
+	states, err := store.GetBackendStates(dbConn, store.NewQuery())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, state := range states {
+		res, _ := json.Marshal(state)
+		fmt.Println(string(res))
+		fmt.Println("L:", state.Latency)
+	}
+
 	cmd := cluster.AddBackend(&cluster.AddBackendRequest{
 		Backend: &bbb.Backend{
-			Host:   "https://bbbackend01.bastelgenosse.de/bigbluebutton/api",
+			Host:   "https://bbbackend01.bastelgenosse.de/bigbluebutton/aapi",
 			Secret: "nwlly97dniqQxKTWHdbGItgieGEBSnyL6s8I3FtM28",
 		},
 		Tags: []string{"sip", "2.0.0"},
