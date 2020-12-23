9bdcecf40983e27fd030258ba4d35a68837ddddb
Annika Hannig <annika@hannig.cc>
2020-12-23 16:09:57 +0100

    added information about version and application date

--- pkg/store/connection.go
+++ pkg/store/connection.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/rs/zerolog/log"
 )
 
 // Connect establishes a database connection and
@@ -35,16 +36,30 @@ func Connect(url string) (*pgxpool.Pool, error) {
 // AssertDatabaseVersion tests if the current
 // version of the database is equal to a required version
 func AssertDatabaseVersion(pool *pgxpool.Pool, version int) error {
-	var current int
+	var (
+		current   int
+		appliedAt time.Time
+	)
+
 	ctx, cancel := context.WithTimeout(
 		context.Background(), time.Second)
 	defer cancel()
 
-	qry := `SELECT MAX(version) FROM __meta__`
-	err := pool.QueryRow(ctx, qry).Scan(&current)
+	qry := `
+		SELECT version, applied_at
+		  FROM __meta__
+		 ORDER BY version DESC
+		 LIMIT 1
+	`
+	err := pool.QueryRow(ctx, qry).Scan(&current, &appliedAt)
 	if err != nil {
 		return err
 	}
+
+	log.Info().
+		Int("version", current).
+		Time("appliedAt", appliedAt).
+		Msg("checking database schema")
 
 	if current != version {
 		return fmt.Errorf(
