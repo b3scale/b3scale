5e007df08c76e2a3dca01e7c46a44ad10a882541
Annika Hannig <annika@hannig.cc>
2021-01-14 19:09:40 +0100

    fixed typo

--- pkg/cluster/backend.go
+++ pkg/cluster/backend.go
@@ -408,7 +408,7 @@ func (b *Backend) GetMeetings(
 	mstates, err := store.GetMeetingStates(b.pool, store.Q().
 		Join("frontends ON frontends.id = meetings.frontend_id").
 		Where("meetings.backend_id IS NOT NULL").
-		Where("frontend.key = ?", req.Frontend))
+		Where("frontends.key = ?", req.Frontend))
 	if err != nil {
 		return nil, err
 	}
