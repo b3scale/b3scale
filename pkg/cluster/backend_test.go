b05cc3a0354de6638b7d9d038de75e596368199b
Annika Hannig <annika@hannig.cc>
2021-02-08 17:33:11 +0100

    test stress calculation

---
+++ pkg/cluster/backend_test.go
@@ -0,0 +1,23 @@
+package cluster
+
+import (
+	"testing"
+
+	"gitlab.com/infra.run/public/b3scale/pkg/store"
+)
+
+func TestBackendStress(t *testing.T) {
+	b := BackendFactory(&store.BackendState{
+		ID:             "A",
+		MeetingsCount:  10,
+		LoadFactor:     1,
+		AttendeesCount: 12,
+	})
+	if b.Stress() != 22 {
+		t.Error("unexpected result for stress function:", b.Stress())
+	}
+	b.state.LoadFactor = 1.25
+	if b.Stress() != 27.5 {
+		t.Error("unexpected result for stress function:", b.Stress())
+	}
+}
