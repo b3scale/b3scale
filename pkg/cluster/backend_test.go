ee92388dd1b9fc4cf41b73d5b8ae99b849666cc8
Annika Hannig <annika@hannig.cc>
2021-04-12 16:12:42 +0200

    check presence of tags

--- pkg/cluster/backend_test.go
+++ pkg/cluster/backend_test.go
@@ -21,3 +21,63 @@ func TestBackendStress(t *testing.T) {
 		t.Error("unexpected result for stress function:", b.Stress())
 	}
 }
+
+func TestBackendHasTag(t *testing.T) {
+	be := &Backend{
+		state: &store.BackendState{
+			Tags: []string{"foo", "bar"},
+		},
+	}
+
+	if !be.HasTag("foo") {
+		t.Error("backend should have tag foo")
+	}
+
+	if be.HasTag("baz") {
+		t.Error("backend should not have tag baz")
+	}
+
+	be = &Backend{
+		state: &store.BackendState{
+			Tags: nil,
+		},
+	}
+
+	if be.HasTag("foo") {
+		t.Error("backend should not have tag foo")
+	}
+}
+
+func TestBackendHasTags(t *testing.T) {
+	be := &Backend{
+		state: &store.BackendState{
+			Tags: []string{"foo", "bar", "baz"},
+		},
+	}
+
+	if !be.HasTags(nil) {
+		t.Error("any tag might be present")
+	}
+
+	if !be.HasTags([]string{"foo", "baz"}) {
+		t.Error("should have tags foo, baz")
+	}
+
+	if be.HasTags([]string{"brrz"}) {
+		t.Error("should not have tags brrz")
+	}
+
+	be = &Backend{
+		state: &store.BackendState{
+			Tags: nil,
+		},
+	}
+
+	if !be.HasTags(nil) {
+		t.Error("any tag might be present")
+	}
+
+	if be.HasTags([]string{"foo"}) {
+		t.Error("should not have tags foo")
+	}
+}
