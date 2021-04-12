be653f3ce6c6940bfa03b3c5cc8536771acb1666
Annika Hannig <annika@hannig.cc>
2021-04-12 16:39:43 +0200

    use require tags middleware

--- cmd/b3scaled/main.go
+++ cmd/b3scaled/main.go
@@ -68,10 +68,11 @@ func main() {
 	// Initialize cluster
 	ctrl := cluster.NewController()
 
-	// Start router
+	// Create router and configure middlewares.
+	// The middlewares are executes in reverse order.
 	router := cluster.NewRouter(ctrl)
 	router.Use(routing.SortLoad)
-	router.Use(routing.RequireTags)
+	router.Use(routing.RequiredTags)
 
 	// Start cluster request handler, and apply middlewares.
 	// The middlewares are executes in reverse order.
