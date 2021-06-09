8a55f519bb2cb064b4f1cd7170090db37057fc05
Annika Hannig <annika@hannig.cc>
2021-06-09 18:35:50 +0200

    added frontends destroy and update

--- pkg/http/api/v1/frontends.go
+++ pkg/http/api/v1/frontends.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog/log"
+	//	"github.com/rs/zerolog/log"
 
 	"gitlab.com/infra.run/public/b3scale/pkg/store"
 )
@@ -33,8 +33,8 @@ func FrontendsList(c echo.Context) error {
 func FrontendCreate(c echo.Context) error {
 	ctx := c.(*APIContext)
 	cctx := ctx.Ctx()
-	accountRef := ctx.AccountRef()
 	isAdmin := ctx.HasScope(ScopeAdmin)
+	accountRef := ctx.AccountRef()
 
 	f := &store.FrontendState{}
 	if err := c.Bind(f); err != nil {
@@ -47,6 +47,12 @@ func FrontendCreate(c echo.Context) error {
 		Active:   f.Active,
 	})
 
+	if isAdmin {
+		frontend.AccountRef = f.AccountRef
+	} else {
+		frontend.AccountRef = &accountRef
+	}
+
 	if err := frontend.Validate(); err != nil {
 		return err
 	}
@@ -57,24 +63,146 @@ func FrontendCreate(c echo.Context) error {
 	}
 	defer tx.Rollback(cctx)
 
-	return nil
+	if err := frontend.Save(cctx, tx); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(cctx); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, frontend)
 }
 
 // FrontendRetrieve will retrieve a single frontend
 // identified by ID.
 func FrontendRetrieve(c echo.Context) error {
-	return nil
+	ctx := c.(*APIContext)
+	cctx := ctx.Ctx()
+	isAdmin := ctx.HasScope(ScopeAdmin)
+	accountRef := ctx.AccountRef()
+	id := c.Param("id")
+
+	tx, err := store.ConnectionFromContext(cctx).Begin(cctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(cctx)
+
+	q := store.Q().Where("id = ?", id)
+	if !isAdmin {
+		q = q.Where("account_ref = ?", accountRef)
+	}
+
+	frontend, err := store.GetFrontendState(cctx, tx, q)
+	if err != nil {
+		return err
+	}
+
+	if frontend == nil {
+		return echo.ErrNotFound
+	}
+
+	return c.JSON(http.StatusOK, frontend)
 }
 
 // FrontendDestroy will remove a frontend from the cluster.
 // The frontend is identified by ID.
 func FrontendDestroy(c echo.Context) error {
-	return nil
+	ctx := c.(*APIContext)
+	cctx := ctx.Ctx()
+	isAdmin := ctx.HasScope(ScopeAdmin)
+	accountRef := ctx.AccountRef()
+	id := c.Param("id")
+
+	tx, err := store.ConnectionFromContext(cctx).Begin(cctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(cctx)
+
+	q := store.Q().Where("id = ?", id)
+	if !isAdmin {
+		q = q.Where("account_ref = ?", accountRef)
+	}
+
+	frontend, err := store.GetFrontendState(cctx, tx, q)
+	if err != nil {
+		return err
+	}
+
+	if frontend == nil {
+		return echo.ErrNotFound
+	}
+
+	if err := frontend.Delete(cctx, tx); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(cctx); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, frontend)
 }
 
 // FrontendUpdate will update the frontend with values
 // provided by the request. Only keys provided will
 // be updated.
 func FrontendUpdate(c echo.Context) error {
-	return nil
+	ctx := c.(*APIContext)
+	cctx := ctx.Ctx()
+	isAdmin := ctx.HasScope(ScopeAdmin)
+	accountRef := ctx.AccountRef()
+	id := c.Param("id")
+
+	tx, err := store.ConnectionFromContext(cctx).Begin(cctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(cctx)
+
+	q := store.Q().Where("id = ?", id)
+	if !isAdmin {
+		q = q.Where("account_ref = ?", accountRef)
+	}
+
+	frontend, err := store.GetFrontendState(cctx, tx, q)
+	if err != nil {
+		return err
+	}
+	if frontend == nil {
+		return echo.ErrNotFound
+	}
+
+	update, err := store.GetFrontendState(cctx, tx, q)
+	if err != nil {
+		return err
+	}
+
+	if err := c.Bind(update); err != nil {
+		return err
+	}
+
+	// Update fields
+	frontend.Frontend = update.Frontend
+	frontend.Active = update.Active
+	frontend.Settings = update.Settings
+
+	if isAdmin {
+		frontend.AccountRef = update.AccountRef
+	}
+
+	if err := frontend.Validate(); err != nil {
+		return err
+	}
+	if err := frontend.Save(cctx, tx); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(cctx); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, frontend)
 }
