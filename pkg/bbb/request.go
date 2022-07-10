b2cd00ee95cb2491ee5a53c97b21770fa857b9b3
Annika Hannig <annika@hannig.cc>
2022-07-10 17:02:09 +0200

    omit empty values when serialized as json

--- pkg/bbb/request.go
+++ pkg/bbb/request.go
@@ -56,6 +56,18 @@ func (p Params) String() string {
 	return strings.Join(q, "&")
 }
 
+// MarshalJSON is a custom implementation of the
+// interface to omit empty values.
+func (p Params) MarshalJSON() ([]byte, error) {
+	filtered := map[string]string{}
+	for k, v := range p {
+		if v != "" {
+			filtered[k] = v
+		}
+	}
+	return json.Marshal(filtered)
+}
+
 // MetaParam creates a meta parameter. In practice
 // this is prefixing `meta_`.
 func MetaParam(name string) string {
