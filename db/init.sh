ab56f02fd7ae1049a14ef7aa88edf9b3e90a3e2a
Annika Hannig <annika@hannig.cc>
2022-08-09 10:00:14 +0200

    lets see if CI works...

--- db/init.sh
+++ db/init.sh
@@ -12,32 +12,32 @@ if [ -z $PSQL ]; then
     PSQL="psql"
 fi
 
-if [ -z $DB_HOST ]; then
-    DB_HOST="localhost"
+if [ -z $PGHOST ]; then
+    PGHOST="localhost"
 fi
 
-if [ -z $DB_PORT ]; then
-    DB_PORT="5432"
+if [ -z $PGPORT ]; then
+    PGPORT="5432"
 fi
 
-if [ -z $DB_NAME ]; then
-    DB_NAME="b3scale"
+if [ -z $PGDATABASE ]; then
+    PGDATABASE="b3scale"
 fi
 
 if [ -z $DB_USER ]; then
-    DB_USER="postgres"
+    PGUSER="postgres"
 fi
 
 if [ -z $DB_PASSWORD ]; then
-    DB_PASSWORD="postgres"
+    PGPASSWORD="postgres"
 fi
 
 ## Setup postgres env
-export PGHOST=$DB_HOST
-export PGPORT=$DB_PORT
-export PGDATABASE=$DB_NAME
-export PGUSER=$DB_USER
-export PGPASSWORD=$DB_PASSWORD
+export PGHOST=$PGHOST
+export PGPORT=$PGPORT
+export PGDATABASE=$PGDATABASE
+export PGUSER=$PGUSER
+export PGPASSWORD=$PGPASSWORD
 
 
 ## Commandline opts: 
